9e2ae75db8c3be6c73119f17b8166f694a9c436c
Zsolt Szalma <zsolt.szalma@gmail.com>
2022-05-23 21:40:24 +0200

    Setup express server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Sidewalk Astronomy');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+host = '127.0.0.1';
+
+app.listen(port, host, console.log(`Server is listening on PORT ${port}...`))
